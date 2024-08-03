e8947f55306517d3e8222553ee038caffc57fdb1
LupinaIgor <igorlupina222@gmail.com>
2024-08-03 14:16:24 +0300

    project Sailor

---
+++ SailorWebServer/SailorWebServer/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace SailorWebServer.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
